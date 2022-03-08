3a3488b99385c1aa2f10c72b9f0e4c539be915e7
tylusdawkins <tylusjdawkins@gmail.com>
2022-03-08 09:01:43 -0800

    Log and Attack added

--- app.js
+++ app.js
@@ -1,4 +1,9 @@
 // Variables
+const logDisplay = document.querySelector('#log')
+let log = []
+logDisplay.innerText = `${log[0]}`
+
+
 class Enemy {
     constructor(enemyName,healthPoints,attack,defense){
         this.enemyName=enemyName
@@ -11,23 +16,54 @@ class Enemy {
 let hero = {
     heroName: "Hero",
     healthPoints: 250,
-    attack:10,
+    strength:10,
     defense:10,
     MP: 100,
 }
 
-const goblin = new Enemy("Goblin",50,5,0)
+const goblin = new Enemy("Goblin",50,5,2)
 console.log(goblin)
+
+
+
+
+
+const heroHpDisplay = document.querySelector('#herohp')
+const heroHp = hero.healthPoints
+heroHpDisplay.innerText = `${heroHp} HP`
+
+const enemyHpDisplay = document.querySelector('#enemyhp')
+let enemyHp = goblin.healthPoints
+enemyHpDisplay.innerText = `${enemyHp} HP`
 
 // Fighting Logic
 let turn = 1
 
+let playerTurn = () => {
+    if(0 < enemyHp){
+        enemyHp -= hero.strength-goblin.defense
+        enemyHpDisplay.innerText= enemyHp
+        if(0> enemyHp){
+            enemyHp=0
+            enemyHpDisplay.innerText= enemyHp
+        }
+    }
+    else enemyHp=0, enemyHpDisplay.innerText= enemyHp
+
+}
 // if(turn=1){
 //     player
 // }
 
 
 //Misc
+
+
+//Queries and Event Listenters
+const attackB = document.querySelector('#attack')
+attackB.addEventListener('click',playerTurn)
+
+
 
 document.onkeydown = function (event) {
     switch (event.keyCode) {
---
+++ scratch.js
@@ -0,0 +1,5 @@
+let playerTurn = (playerAction, enemyAction) => {
+    if(0 < enemyHp)
+    enemyHp -= 10-goblin.defense
+    enemyHpDisplay.innerText= enemyHp
+}
